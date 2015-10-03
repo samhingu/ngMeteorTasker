e6800f171f5960bd0bc934614e456c40f5a7db14
Sumit Hingu <sp.hingu@gmail.com>
2015-10-03 10:59:46 -0700

    Implemented filter and count for task

--- AngMetBySam.js
+++ AngMetBySam.js
@@ -8,8 +8,9 @@ if (Meteor.isClient) {
       function ($scope, $meteor) {
 
           $scope.tasks = $meteor.collection(function () {
-              return Tasks.find({}, { sort: { createdAt: -1 } });
+              return Tasks.find($scope.getReactively('query'), { sort: { createdAt: -1 } });
           });
+
           $scope.addTask = function (newTask) {
               $scope.tasks.push({
                   text: newTask,
@@ -17,5 +18,16 @@ if (Meteor.isClient) {
               });
           }
 
+          $scope.$watch('hideCompleted', function () {
+              if ($scope.hideCompleted) {
+                  $scope.query = { checked: { $ne: true } };
+              } else {
+                  $scope.query = {};
+              }
+          });
+
+          $scope.incompleteCount= function(){
+            return Tasks.find({checked :{$ne: true}}).count();
+          };
       }]);
 }
